e82611d9a0b9bb5f60f9f88ad6285f8a2b7a98aa
Michael Meyer <michael.c.meyer26@gmail.com>
2020-09-08 14:54:58 -0500

    added a mole that pops into different boxes via recursion

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -1,4 +1,20 @@
 molehills = $('.molehill');
 
-$(molehills[Math.floor(Math.random()*molehills.length)]).addClass('mole');
+// var currentLocation = $(molehills[Math.floor(Math.random()*molehills.length)]);
+// $(currentLocation).addClass('mole');
 
+
+
+function molePop() {
+    var currentLocation = $(molehills[Math.floor(Math.random()*molehills.length)]);
+    $(currentLocation).addClass('mole');
+
+    var delay = 1000; // delay time in milliseconds
+
+    var timeoutId = setTimeout(function () {
+        $(currentLocation).removeClass('mole');
+        molePop()
+    }, delay);
+}
+
+molePop();
