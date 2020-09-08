b43c985be9ac7f192ca03bac0b459a2b1e2bad7a
Michael Meyer <michael.c.meyer26@gmail.com>
2020-09-08 15:55:31 -0500

    cleaned code a bit and tried to bugfix - unsuccessfully

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -1,39 +1,35 @@
-molehills = $('.molehill');
+"use strict"
 
-// var currentLocation = $(molehills[Math.floor(Math.random()*molehills.length)]);
-// $(currentLocation).addClass('mole');
+var molehills = $('.molehill');
 
-var score = 0
+var currentScore = 0;
+var highScore = 0;
+
+$( "#start-button" ).click(molePop(currentScore));
 
 function molePop(score) {
+    //randomly selects a 'molehill' - one of the boxes - and then assigns it as a variable
+    //also gives it the 'mole' class to display the mole.
     var currentLocation = $(molehills[Math.floor(Math.random()*molehills.length)]);
     $(currentLocation).addClass('mole');
 
+    //When the mole is clicked, remove that class ("hiding" the mole) and increment score
+    //then update html to reflect new score
     $(currentLocation).click(function () {
         $(currentLocation).removeClass('mole');
         score++;
-        console.log(score);
-        $('#score').html(score)
-    })
+        $('#current-score').html(score);
+        currentLocation = undefined;
+    });
 
     //makes the mole move around on a timer
+    //when the timer runs out, remove the mole class and then run the function again from the top
     var delay = 1500; // delay time in milliseconds
     var timeoutId = setTimeout(function () {
         $(currentLocation).removeClass('mole');
-        molePop(score)
+        currentLocation = undefined;
+        molePop(score);
     }, delay);
-
-    // var interval = 1000; // interval time in milliseconds
-    //
-    // var intervalId = setInterval(function () {
-    //     if (count >= max) {
-    //         clearInterval(intervalId);
-    //         console.log('All done');
-    //     } else {
-    //         count++;
-    //         console.log('Repeating this line ' + count);
-    //     }
-    // }, interval);
 }
 
-molePop(score);
+
