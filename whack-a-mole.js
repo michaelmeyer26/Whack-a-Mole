cfb63a8c9efcb0c64acad2c19a18d57481bba530
Michael Meyer <michael.c.meyer26@gmail.com>
2020-09-09 12:22:15 -0500

    Initial work on putting in rounds

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -3,6 +3,7 @@
 $(document).ready(function() {
     var molehills = $('.molehill');
 
+    var timeoutId;
     var currentScore = 0;
     var highScore = 0;
 
@@ -24,14 +25,28 @@ $(document).ready(function() {
         //makes the mole move around on a timer
         //when the timer runs out, remove the mole class and then run the function again from the top
         var delay = 1500; // delay time in milliseconds
-        var timeoutId = setTimeout(function () {
+        timeoutId = setTimeout(function () {
             $(currentLocation).removeClass('mole');
-            $(currentLocation).off('click')
+            $(currentLocation).off('click');
             molePop();
         }, delay);
     }
 
+
     $( "#start-button" ).click(molePop);
+
+    //TODO: Figure out a way to have the game end after 30 secs. Below function doesn't work because molePop keeps calling itself I guess?
+    //TODO: fix this by putting everything into one function, that can actually interrupt the 1.5 sec timer molepop is on
+    //TODO: Maybe need to refactor code to use functions to keep things neat
+
+    // $( "#start-button" ).click(function() {
+    //     setTimeout(function () {
+    //         $(currentLocation).removeClass('mole');
+    //         $(currentLocation).off('click');
+    //         clearTimeout(timeoutId);
+    //     }, 30000);
+    //     molePop();
+    // });
 });
 
 
